89ef99682bd11398c5f744abc0bbacdc2fac4893
sushimadeeasy <sushantsrikrish@gmail.com>
2017-11-10 18:07:18 -0800

    Intial layout of HTML and setting up variables on javascript file

---
+++ Psychic-Game/assets/javascript/game.js
@@ -0,0 +1,10 @@
+//Setting up the initial alphabet array to select for guessing 
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//Setting up the variables that will be used to store Wins, Losses, Guesses left, and your guesses
+var wins = 0;
+var loss = 0;
+var guessLeft = 9;
+var yourGuess;
+
+console.log(letters);
