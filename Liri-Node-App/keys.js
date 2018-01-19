b927c25f3cf6a06ce5bc0c402ca262bdc441ee09
sushimadeeasy <sushantsrikrish@gmail.com>
2018-01-18 18:18:06 -0800

    Pushing the Liri Node application files to github

---
+++ Liri-Node-App/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'YJwhnzYWZcrp0HnFcUUPZqtzM',
+  consumer_secret: 'eBqiAS5SzMGyyA6FFBKAZvyUESF3JDPblDlp86hLSmeslGKtyW',
+  access_token_key: '4160124852-JtdWLq3YGCKE1kCSWbDEP9OJkFtvymUU5erAF3N',
+  access_token_secret: '3Je44K8CaIZeE23TgJqCrtmCHjUWZvC2AOv9kRRDigJaE'
+};
